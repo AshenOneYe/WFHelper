718b5412ccdf0b60ffae8f3bb48f5a5f16fdb2e9
puresox <puresox@163.com>
2021-11-03 09:00:19 +0800

    1.把adb集成到程序 2.使用pyinstaller生成EXE文件，直接运行就行

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
+import getopt
+import os
+import sys
+import threading
+
 from Config import config
 from server.Server import Server
 from utils.ADBUtil import adbUtil
-import sys
-import getopt
-import threading
 from utils.LogUtil import Log
 from WFHelper import wfhelper
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    # https://cloud.tencent.com/developer/article/1739886
+    def base_path(path):
+        if getattr(sys, "frozen", None):
+            basedir = sys._MEIPASS
+        else:
+            basedir = os.path.dirname(__file__)
+        return os.path.join(basedir, path)
+
+    tmd = base_path("")  # 这是解压路径
+    cwd = os.getcwd()  # 这是程序的所在路径
+    # 当需要调用打包的外部文件时
+    os.chdir(tmd)  # 先把工作路径变成解压路径
+    # adb连接
+    os.system(r"adb\adb.exe connect 127.0.0.1:7555")
+    # 当需要写出文件到程序所在目录时
+    os.chdir(cwd)  # 把工作路径切换回来
 
     try:
-        opts,args = getopt.getopt(sys.argv[1:], "d:s:c:")
+        opts, args = getopt.getopt(sys.argv[1:], "d:s:c:")
         opts = dict(opts)
 
         if "-d" in opts:
@@ -18,14 +37,14 @@ if __name__ == '__main__':
             adbUtil.setDevice(device)
         else:
             adbUtil.setDevice(None)
-            
+
         if "-s" in opts:
             savePath = opts["-s"]
             Log.info("截图保存至 : {}".format(savePath))
             adbUtil.getScreen(savePath=savePath)
             sys.exit()
         if "-c" in opts:
-            config.setConfigPath(opts["-c"])        
+            config.setConfigPath(opts["-c"])
 
             # TODO -v 参数打印log信息
     except getopt.GetoptError:
@@ -40,4 +59,3 @@ if __name__ == '__main__':
 
     # 不用子线程启动的原因是，子线程莫名的速度慢很多
     wfhelper.start()
-    
