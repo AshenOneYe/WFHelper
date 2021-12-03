1e8d4f8c338a08e91ffca4ebaa5c53aef8bbdaa1
yehaoqi <1282496341@qq.com>
2021-12-03 21:04:43 +0800

    修改validate为if，完善功能

--- WFHelper.py
+++ WFHelper.py
@@ -32,93 +32,15 @@ class WFHelper:
         finally:
             return result
 
-    def checkRequire(self, require):
-        if len(require) == 1:
-            # *** 仅有一个参数时，将其当作变量名进行判断 ***
-            key = self.actionManager.formatArg(require[0])
-            val = self.state.getState(key)
-            if val == 0 or val is None:
-                return False
-            else:
-                return True
-        else:
-            statement = ""
-            for arg in require:
-                statement += "{}".format(self.actionManager.formatArg(arg))
-
-            while True:
-                try:
-                    # FIXME eval 不安全，请使用 aeval 模块代替
-                    return eval(statement, self.state.content)
-
-                except NameError as e:
-
-                    # *** 解析所缺变量名，写法待改进，大概在3.10可以直接用name？ ***
-                    var = e.args[0]
-                    var = var[var.find("'") + 1: var.rfind("'")]
-                    # ***********************************************************
-
-                    self.state.setState(var, 0)
-                    Log.error("缺少变量 : {}. 已赋初值 0".format(var))
-
-                except SyntaxError as e:
-                    Log.error("语法错误 : {}".format(e))
-                    return False
-
-                except TypeError as e:
-                    Log.error("类型错误 : {}".format(e))
-                    return False
-
-                except ArithmeticError as e:
-                    Log.error("运算错误 : {}".format(e))
-                    return False
-
-                except Exception as e:
-                    Log.error("其他错误 : {}".format(e))
-                    return False
-
-            # *** 以下为旧的硬编码实现方法 ***
-
-            # key = self.actionManager.formatArg(require[0])
-            # ope = require[1]
-            # lim = require[2]
-            # if ope == "==":
-            #     return True if self.state.getState(key) == lim else False
-            # else:
-            #     val = 0
-            #     if not self.state.has(key):
-            #         self.state.setState(key, 0)
-            #     else:
-            #         val = self.state.getState(key)
-            #     try:
-            #         val = float(val)
-            #         lim = float(lim)
-            #         if ope == "<":
-            #             return True if val < lim else False
-            #         elif ope == "<=":
-            #             return True if val <= lim else False
-            #         elif ope == ">":
-            #             return True if val > lim else False
-            #         elif ope == ">=":
-            #             return True if val >= lim else False
-            #         else:
-            #             return False
-            #     except TypeError as arg:
-            #         Log.error("错误的边界值 : {}".format(arg))
-            #         return False
-
     def mainLoop(self, targets):
 
         t = int(time.time())
 
         for target in targets:
-            if "require" in target and not self.checkRequire(target["require"]):
-                continue
-
             if "hash" not in target or self.check(target, self.screen):
                 if "hash" not in target:
                     Log.info("{} - 直接操作".format(target["text"]))
-                self.actionManager.doAction(target)
+                self.actionManager.doActions(target)
                 self.updateActionTime(t)
                 return True
 
